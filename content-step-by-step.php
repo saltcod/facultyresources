8158d0f3a5cce188bd69df175483d5eeaf403242
Terry Sutton <saltcod@gmail.com>
2013-03-25 11:07:02 -0230

    Wrap CFS fields in an if

--- content-step-by-step.php
+++ content-step-by-step.php
@@ -13,36 +13,6 @@
 	<div class="breadcrumbs"><?php if(function_exists('bcn_display')) {bcn_display(false, false); }?> </div>
 </header><!-- .entry-header -->
 
-
- 
-<?php // Show Captivate video if guide has one ?> 
-<?php global $cfs; if ( $cfs->get('captivate_url' )) : ?>
-
-	<!-- Button to trigger modal -->
-	<h3 class="watch-video"><a href="#watch-video-button" role="button" class="btn"><i class="icon-facetime-video"></i>Watch a video<i class="icon-chevron-right second"></i></a></h3>
-
-	<!-- Modal -->
-	<div id="watch-video-modal" class="modal hide fade" tabindex="-1">
-
-		<div class="modal-header">
-			<div class="video-title"><?php the_title();?></div> 
-			<a class="close" data-dismiss="modal" aria-hidden="true"><i class="icon-remove"></i></a>
-		</div>
-
-		<div class="modal-body">
-			<?php include('captivate-embed.php'); ?>
-		</div>
-
-		<div class="modal-footer">
-			<a class="btn close" data-dismiss="modal" aria-hidden="true">Close</a>
-		</div>
-
-	</div>
-<?php endif; ?>
-
-
- 
-
 <div class="entry-content">
 	<?php the_content(); ?>
 	<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'victoria_park' ), 'after' => '</div>' ) ); ?>
--- content-tool-landing-page.php
+++ content-tool-landing-page.php
@@ -4,28 +4,41 @@
  * @subpackage Victoria Park
  */
 ?>
+
+
+<?php // Setup custom fields up here
+	global $cfs;
+	$faq = $cfs->get('frequently_asked_questions');
+	$intro = $cfs->get('tool_intro');
+	?>
+
+
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
- 	 
-<?php global $cfs; ?>
-
-<header class="entry-header">
-    <h1 class="entry-title"><?php the_title(); ?></h1>
-    <div class="breadcrumbs"><?php if(function_exists('bcn_display')) {bcn_display(false, false); }?> </div>
-</header><!-- .entry-header -->
- 
-   <div class="landing-page-description"><?php echo $cfs->get('tool_intro'); ?></div> 
-  
-     <?php if( $cfs->get('frequently_asked_questions') ): ?>  
-   		<h3 class="faq">Frequently Asked Questions</h3> 
-     
-    <?php echo $cfs->get('frequently_asked_questions');  ?>
-    <?php endif; ?>
-
-     
-   
-
- 	
-	<?php edit_post_link( __( 'Edit', 'victoria_park' ), '<span class="edit-link">', '</span>' ); ?>
- </article><!-- #post-<?php the_ID(); ?> -->
 
 
+	<header class="entry-header">
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+		<div class="breadcrumbs"><?php if(function_exists('bcn_display')) {bcn_display(false, false); }?> </div>
+	</header><!-- .entry-header -->
+
+
+
+	<div class="landing-page-description"> 
+		<?php if( isset( $intro )) {
+			 echo $intro;
+			}
+		?>
+	</div> 
+	
+	
+
+	<?php if( !empty( $faq )): ?>
+		<h3 class="faq">Frequently Asked Questions</h3> 
+		 <?php echo $faq; ?>
+	<?php endif; ?>
+
+
+
+
+<?php edit_post_link( __( 'Edit', 'victoria_park' ), '<span class="edit-link">', '</span>' ); ?>
+</article><!-- #post-<?php the_ID(); ?> -->
