cfe14d572480e4c1a9276627f41096aca99ea204
Terry Sutton <saltcod@gmail.com>
2013-05-27 16:42:57 -0230

    Make sidebar functions generic

--- sidebar.php
+++ sidebar.php
@@ -28,7 +28,7 @@
 		'title_li'     => __(''),
 		'echo' => 0,
 		'child_of' => $parent //The ID of the top D2L guide
-			);
+		);
 
 	$children = wp_list_pages( $args );
 	?>
@@ -40,29 +40,38 @@
 		<nav id="menu-sidebar">
 			<ul class="menu">
 				
+				<?php
+				// Return the topmost Parent to the top of the menu stack
+				$page_data = get_page( $parent );
+				echo '<a href="'. get_permalink( $parent ) .'"><h4 class="parent-tool">'. $page_data->post_title .'</h4></a> ';
+				?>
+
 				<?php if( ! is_single(3029) ): ?>
-					<span class="back"><a href="<?php echo home_url();?>/guides/desire2learn/"><i class="icon-chevron-left"></i>Back</a> </span>
-				 <?php endif; ?>
-				
-				<?php echo $children; ?>
-			</ul>
-		</nav>
-	</div>
+				<span class="back"><a href="<?php echo get_permalink( $parent );?>"><i class="icon-chevron-left"></i>Back</a> </span>
+			<?php endif; ?>
+
+			<?php echo $children; ?>
+		</ul>
+	</nav>
+</div>
 
 <?php endif;?>
 
- 
-	<div id="share">
-		<h2>Share</h2>
-		<i class="icon-share"> </i><?php echo direct_email();?>
 
 
-		<?php if( has_term('step-by-step', 'guide-type') ) : ?>
-		<div class="print">
-			<i class="icon-print"></i> <a href="javascript:window.print()">Print this guide</a>
 
-		</div>
-	<?php endif; ?>
+
+
+<div id="share">
+	<h2>Share</h2>
+	<i class="icon-share"> </i><?php echo direct_email();?>
+
+
+	<?php if( has_term('step-by-step', 'guide-type') ) : ?>
+	<div class="print">
+		<i class="icon-print"></i> <a href="javascript:window.print()">Print this guide</a>
+	</div>
+<?php endif; ?>
 
 </div>
 
